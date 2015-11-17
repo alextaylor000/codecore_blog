353d6d4979108628e4fb5f1f8a4a58d1fcbf2eef
Alex Taylor <alexmctaylorpants@gmail.com>
2015-11-16 19:15:04 -0800

    Latest scroll implementation - by coordinate offset

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,22 +1,20 @@
 var ready = function() {
-  var i = 2; // incrementor for the next arrow
+  //var i = 2; // incrementor for the next arrow
   var postShowcaseHeight = $(".post-showcase").height();
-  var previousScroll =
-  $(window).on("scroll",function(event){
+  function getCurrentPostId(){
     var scrollPos = $(document).scrollTop();
-    var atId = Math.ceil(scrollPos / postShowcaseHeight);
-    //var atId = Math.round(scrollPos / postShowcaseHeight) + 1;
-    console.log(atId);
-    i = atId;
-  });
-
+    console.log("current scrollPos = ", scrollPos);
+    var atId = Math.ceil((scrollPos / postShowcaseHeight)) + 1;
+    return atId;
+  };
 
   // scroll to the next post
   $(".next-arrow").on("click",function() {
+    var curPostId = getCurrentPostId();
+    console.log("currently at ", curPostId, " scrolling to ", curPostId);
     var p = $(this);
-    var newOffset = $(".posts .post-showcase:nth-child(" + i + ")").offset()
-    $("html, body").animate({scrollTop: (newOffset.top - 200)});
-    i++;
+    var newOffset = $(".posts .post-showcase:nth-child(" + (curPostId) + ")").offset();
+    $("html, body").animate({scrollTop: (newOffset.top)});
   });
 };
 
